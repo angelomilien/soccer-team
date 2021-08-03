4a42e4b92a5ee3feaa941ac7e9f205d6c37e0535
Angelo <angelomilienjoseph@gmail.com>
2021-08-03 18:55:00 -0400

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
     has_many :teams, through: :players, :foreign_key => 'coach_id'
 
     has_secure_password
-end
+
+    validates :user_name, :email, presence: true
+    validates :user_name, :email, uiqueness: true
 
 # --no-test-framework
