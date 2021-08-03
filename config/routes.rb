adf9ad0f846bbf9039d03bd321917f8cc8e159e6
Angelo <angelomilienjoseph@gmail.com>
2021-08-03 15:43:26 -0400

    added a few routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+    
+    root 'sessions#home'
+    get 'signup', to: 'user#new'
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    resources :players
+    resources :teams
+    resources :countries
+    resources :users
+    # get 'about', to: 'static#about'
+  # resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 end
