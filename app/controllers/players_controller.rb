926476d6a056589924eefea86e244dba04379516
Angelo <angelomilienjoseph@gmail.com>
2021-08-23 06:55:50 -0400

    nested route added

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -6,16 +6,26 @@ class PlayersController < ApplicationController
 
 
     def new
-        @player = Player.new
-        @player.build_team
+        if params[:team_id] && @team = Team.find_by_id(params[:team_id])
+    
+            if current_user.teams.include? @team
+                @player = @team.players.build
+            else
+                flash[:message] = "you have been redirected"
+                redirect_to players_path
+            end
+        else
+            @player = Player.new
+            @player.build_team
+        end
     end
     
     def index
-        if params[:team_id]
-            @team = Team.find_by_id(params[:team_id])
+        if params[:team_id] && @team = Team.find_by_id(params[:team_id])
             if current_user.teams.include? @team
                @players = @team.players
             else
+                flash[:message] = "you have been redirected"
                 redirect_to players_path
             end
         else
@@ -32,6 +42,7 @@ class PlayersController < ApplicationController
 
     
     def create
+    
         @player = Player.new(player_params)
         if @player.save
             redirect_to player_path(@player)
@@ -59,7 +70,7 @@ class PlayersController < ApplicationController
 
 
     def player_params
-        params.require(:player).permit(:name, :age, :position, :number, :coach_id, team_attributes: [:name, :country_name])
+        params.require(:player).permit(:name, :age, :team_id, :position, :number, :coach_id, team_attributes: [:name, :country_name])
     end
 
     def find_player 
