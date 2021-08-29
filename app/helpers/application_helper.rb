da4e9860452ec7d3c406c080a2f93d31e9a41aea
Angelo <angelomilienjoseph@gmail.com>
2021-08-29 16:53:49 -0400

    added css on all page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,9 +34,9 @@ module ApplicationHelper
 
     def navigation_helper 
         if !logged_in?
-            button_to("Sign in with Google", '/auth/google_oauth2', style: "background-color: #dd4b39;", class: "btn btn-lg btn-block btn-primary fab fa-google me-2") 
+            button_to("Sign in with Google", '/auth/google_oauth2', style: "background-color: #dd4b39;", class: "btn btn-lg btn-block btn-primary") 
         else 
-            button_to("Log Out", logout_path, method: 'delete')
+            button_to("Log Out", logout_path, method: 'delete', class: "btn btn-dark btn-lg btn-block")
         end
     end
 
@@ -64,3 +64,61 @@ module ApplicationHelper
     #     end 
     # end
 end
+
+
+
+# <section class="vh-100" style="background-color: #476072;">
+#    <div class="container py-5 h-100">
+#       <div class="row d-flex justify-content-center align-items-center h-100">
+#          <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+#             <div class="card shadow-2-strong" style="border-radius: 1rem;">
+#                <div class="card-body p-5 text-center">
+#                   <h1>Signup here<h1><br>
+   
+#                   <%= render partial: "errors", locals: {object: @user} %>
+   
+#                   <%= form_for @user, url: signup_path do |f|%>
+
+#                   <div class="form-outline mb-4">
+#                      <%= f.label :username %>
+#                      <%= f.text_field :user_name, class: 'form-control form-control-lg'%><br>
+#                   </div>  
+
+#                      <%= render partial: "input", locals: {f: f} %>
+#                   <%end%>
+   
+#                   <%=button_to_welcome_home_page%>
+#                   <%=navigation_helper%>
+#                </div>
+#             </div>
+#          </div>
+#       </div>
+#    </div>
+# </section>
+
+
+
+
+# <section class="vh-100" style="background-color: #476072;">
+#    <div class="container py-5 h-100">
+#       <div class="row d-flex justify-content-center align-items-center h-100">
+#          <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+#             <div class="card shadow-2-strong" style="border-radius: 1rem;">
+#                <div class="card-body p-5 text-center">
+#                   <h1>Login here</h1><br>
+            
+#                   <%= flash[:error]%><br><br>
+            
+#                   <%= form_for :user, url: signing_path do |f|%>
+                     
+#                      <%= render partial: "users/input", locals: {f: f} %>
+#                   <%end%><br>
+   
+#                   <%=button_to_welcome_home_page%><br>
+#                   <%=navigation_helper%>
+#                </div>
+#             </div>
+#          </div>
+#       </div>
+#    </div>
+# </section>
